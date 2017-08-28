053df811db589ad39738bed3c1a765b836d6b3fd
Zacharias Manuel <zacflys@gmail.com>
2017-08-28 13:01:23 +0530

    Edited views

--- aryabot/views.py
+++ aryabot/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+@csrf_exempt
 class ChatterBotAppView(TemplateView):
     template_name = "app.html"
